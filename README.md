e7e33e121b578a73a99040ddf91ddb4d78e6fee3
Wyatt Preul <wpreul@gmail.com>
2016-06-03 22:22:08 -0500

    Update with home search

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # find-rc
-Find a .rc file given a name
+Find a `rc` file given a name.  Inspiration from [rc](https://github.com/dominictarr/rc).
 
-### `(filename, [startDir], callback)`
+The callback is executed on the first found file.  Here is the order that
+folders will be searched:
+1. Current directory
+2. Parent of current directory, until the root folder is encountered
+3. $HOME/.${apprc}
+4. $HOME/.${app}/config
+5. $HOME/.config/${app}
+6. $HOME/.config/${app}/config
+7. /etc/${app}rc
+8. /etc/${app}.rc
 
-- `filename` - name of file you are looking for.  Example: `.labrc`
+
+### `(appname, [startDir], callback)`
+
+- `appname` - name of file you are looking for.  Example: `lab`.  It will be formatted with `.{appname}rc`
 - `startDir` - (optional) directory to start looking for the file.  Defaults to `process.cwd`
 - `callback` - callback function to receive result.  Signature is `(err, filePath)`
 
@@ -12,7 +24,7 @@ Example
 ```js
 const FindRc = require('find-rc');
 
-FindRc('.labrc', (err, filePath) => {
+FindRc('lab', (err, filePath) => {
   if (filePath) {
     // load file and parse configuration
   }
--- test/index.js
+++ test/index.js
@@ -9,28 +9,38 @@ const describe = lab.describe;
 const it = lab.it;
 const expect = Code.expect;
 
-it('finds a file in the current directory', (done) => {
-  FindRc('package.json', (err, filePath) => {
+it('finds an rc in the current working directory', (done) => {
+  FindRc('find', (err, filePath) => {
     expect(err).to.not.exist();
     expect(filePath).to.exist();
-    expect(filePath).to.contain('package.json');
+    expect(filePath).to.contain('.findrc');
     done();
   });
 });
 
 
 it('finds a file in a parent directory', (done) => {
-  FindRc('package.json', __dirname, (err, filePath) => {
+  FindRc('find', __dirname, (err, filePath) => {
     expect(err).to.not.exist();
     expect(filePath).to.exist();
-    expect(filePath).to.contain('package.json');
+    expect(filePath).to.contain('.findrc');
     done();
   });
 });
 
 
 it('reports an error when a file isn\'t found', (done) => {
-  FindRc('.no_way_will_this_exist', (err, filePath) => {
+  FindRc('no_way_will_this_exist', (err, filePath) => {
+    expect(err).to.exist();
+    expect(filePath).to.not.exist();
+    done();
+  });
+});
+
+it('reports an error when a file isn\'t found and env.USERPROFILE is set', (done) => {
+  process.env.USERPROFILE = process.env.HOME;
+  delete process.env.HOME;
+  FindRc('no_way_will_this_exist', (err, filePath) => {
     expect(err).to.exist();
     expect(filePath).to.not.exist();
     done();
