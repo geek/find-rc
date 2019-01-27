743194a907ec12d53b58fbe60e4cd5bd30140187
geek <wpreul@gmail.com>
2019-01-27 14:45:10 -0600

    Update to latest lab

--- test/index.js
+++ test/index.js
@@ -10,32 +10,28 @@ const it = lab.it;
 const expect = Code.expect;
 
 
-it('finds an rc in the current working directory', (done) => {
+it('finds an rc in the current working directory', () => {
   const filePath = FindRc('find');
   expect(filePath).to.exist();
   expect(filePath).to.contain('.findrc.js');
-  done();
 });
 
 
-it('finds a file in a parent directory', (done) => {
+it('finds a file in a parent directory', () => {
   const filePath = FindRc('find', __dirname);
   expect(filePath).to.exist();
   expect(filePath).to.contain('.findrc.js');
-  done();
 });
 
 
-it('returns undefined when a file isn\'t found', (done) => {
+it('returns undefined when a file isn\'t found', () => {
   const filePath = FindRc('no_way_will_this_exist');
   expect(filePath).to.not.exist();
-  done();
 });
 
-it('returns undefined when a file isn\'t found and env.USERPROFILE is set', (done) => {
+it('returns undefined when a file isn\'t found and env.USERPROFILE is set', () => {
   process.env.USERPROFILE = process.env.HOME;
   delete process.env.HOME;
   const filePath = FindRc('no_way_will_this_exist');
   expect(filePath).to.not.exist();
-  done();
 });
